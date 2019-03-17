f504d0f45867122453b4b3dc982a0d45b7e76cb2
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-03-17 23:31:29 +0900

    first public release

---
+++ README.md
@@ -0,0 +1,6 @@
+Tabs2Links
+===
+
+https://chrome.google.com/webstore/detail/tabs2links/hlmnkbgbionilbjkgmghmdeokakmojmi
+
+Chrome Extension which lets you create Markdown links from Chrome's current window's tabs.
--- src/popup/Config.ts
+++ src/popup/Config.ts
@@ -1,7 +1,7 @@
 const Config = {
     DEVELOPER_NAME: "@yslibnet(yslibrary.net)",
     DEVELOPER_URL: "https://twitter.com/yslibnet",
-    CHROME_WEBSTORE_URL: "https://chrome.google.com/webstore/detail",
+    CHROME_WEBSTORE_URL: "https://chrome.google.com/webstore/detail/tabs2links/hlmnkbgbionilbjkgmghmdeokakmojmi",
     APP_VERSION: chrome.runtime.getManifest().version
 }
 
